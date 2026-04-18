34cc3ae295d7ccd1fc8ad181dda6ff226ede9997
Michael Okolo <michaelokolo62@yahoo.com>
2026-04-19 00:36:39 +0100

    revamp the feature marketing and CTA section

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import Hero from '@/components/Hero';
 import Marquee from '@/components/Marquee';
 import FeatureStorefront from '@/components/FeatureStorefront';
 import FeatureDashboard from '@/components/FeatureDashboard';
-import FeatureMarket from '@/components/FeatureMarket';
-import ClosingCTA from '@/components/Closing';
+// import FeatureMarket from '@/components/FeatureMarket';
+// import ClosingCTA from '@/components/Closing';
+import FeatureMarketCTA from '@/components/FeatureMarketCTA_v1';
 import Footer from '@/components/Footer';
 
 export default function Home() {
@@ -15,8 +16,9 @@ export default function Home() {
       <Marquee />
       <FeatureStorefront />
       <FeatureDashboard />
-      <FeatureMarket />
-      <ClosingCTA />
+      {/* <FeatureMarket />
+      <ClosingCTA /> */}
+      <FeatureMarketCTA />
       <Footer />
     </main>
   );
